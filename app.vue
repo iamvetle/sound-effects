826075e4b60aa67a9ec7b0cee65829ea0af5e1fe
Iamvetle <vetle122@gmail.com>
2024-05-31 20:32:38 +0200

    First commit

---
+++ app.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<SoundEffect
+			class="soundeffect"
+			v-for="sound in listOfSoundeffects"
+			:soundeffect="sound"
+			:key="sound.src"
+		/>
+	</div>
+</template>
+
+<script setup lang="ts">
+	const listOfSoundeffects = [
+		{
+			title: "Impressed audience",
+			src: "/audience-impressed-laugh.mp3",
+		},
+		{
+			title: "Sitcom laughter",
+			src: "/sitcom-audience-laugh.mp3",
+		},
+	];
+</script>
+
+<style scoped>
+	.soundeffect {
+		/** Have to do that when it is block - not margin left and right at auto */
+		text-align: center;
+		margin-top: 1em;
+		display: block;
+	}
+</style>
---
+++ components/SoundEffect.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		<audio ref="audioRef" :src="soundeffect.src"></audio>
+		<audio-button @click="playAudio"
+			>Click to play {{ soundeffect.title }}</audio-button
+		>
+	</div>
+</template>
+
+<script setup lang="ts">
+	// Fetches the sound effect that it should have
+
+	const audioRef = ref<HTMLAudioElement | null>(null);
+
+	// Stores the actual sound (url to sound)
+
+	type SoundEffect = {
+		title: string;
+		src: string;
+	};
+
+	const props = defineProps<{
+		soundeffect: SoundEffect;
+	}>();
+
+	// const propSoundeffect = toRef(() => props.soundeffect);
+
+	// onMounted(async () => {
+	// 	const response = (await $fetch(propSoundeffect.value.src)) as Blob;
+
+	// 	const url = URL.createObjectURL(response);
+	// 	sound.value = url;
+	// 	console.log("url for sound", url);
+	// 	console.log("entire response with $fetch", response);
+	// });
+
+	function playAudio() {
+		audioRef.value?.play();
+	}
+</script>
+
+<style>
+	audio {
+		display: none;
+	}
+</style>
