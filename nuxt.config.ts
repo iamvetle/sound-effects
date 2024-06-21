8a7a46ced883813732d75a21a949c14a15c434e3
iamvetle <vetle122@gmail.com>
2024-06-21 13:59:26 +0200

    Fix PWA for the app through vite-pwa.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-
-    nitro: {
-		prerender: {
-			// Have to specify this (npx nuxi generate) or manifest.json will become a html file..
-			ignore: ["/manifest.json"],
+	devtools: { enabled: true },
+	modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],
+	// nitro: {
+	// 	prerender: {
+	// 		// Have to specify this (npx nuxi generate) or manifest.json will become a html file..
+	// 		ignore: ["/manifest.json"],
+	// 	},
+	// },
+	pwa: {
+		minify: false,
+		injectManifest: {
+			maximumFileSizeToCacheInBytes: undefined,
 		},
+		registerType: "autoUpdate",
+		manifestFilename: "manifest.json",
+		workbox: {
+			navigateFallback: "/",
+			// ! THIS IS WHAT MADE IT WORK -> including svg, png and ico in "globPatterns"
+			globPatterns: ["**/*.{js,wasm,css,html,svg,png,ico}"],
+		},
+		devOptions: { enabled: true, type: "module" },
 	},
 
-    modules: ["@nuxtjs/tailwindcss"]
+	// nitro: {
+	// 	prerender: {
+	// 		// Have to specify this (npx nuxi generate) or manifest.json will become a html file..
+	// 		ignore: ["/manifest.json"],
+	// 	},
+	// },
+	ssr: false,
 });
